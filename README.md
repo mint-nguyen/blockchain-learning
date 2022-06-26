a06900f4935604349b831a3b1ebce01f4fd78852
mintnguyen.pa <mintnguyen.pa@gmail.com>
2022-06-25 19:23:22 -0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+- How to run
+
+* Clone this repo
+* Open Remix
+* Import this repo
+* Compile and deploy
---
+++ contracts/FundMe.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+  using PriceConverter for uint256;
+
+    uint256 public constant minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor(){
+      owner = msg.sender;
+    }
+
+    function fund() public payable{
+        require( msg.value.getConversionRate() >= minimumUsd, "Not enough minimum requirement!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+      for( uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        address funder = funders[funderIndex];
+        addressToAmountFunded[funder] = 0;
+      }
+
+      funders = new address[](0);
+
+      payable(msg.sender).transfer(address(this).balance);
+
+      bool sendSuccess = payable(msg.sender).send(address(this).balance);
+      require(sendSuccess, "Send failed!");
+
+      (bool callSuccess, ) =  payable(msg.sender).call{value: address(this).balance}("");
+      require(callSuccess, "Call failed!");
+    }
+
+    modifier onlyOwner {
+      require(msg.sender == owner, "Sender is not owner");
+      _;
+    }
+}
