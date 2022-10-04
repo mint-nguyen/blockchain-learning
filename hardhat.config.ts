8ccb9159972c7754a437517d7beb5aa1c4721ac4
mint-ng <mintnguyen.pa@gmail.com>
2022-10-03 17:05:13 -0700

    feat: deploy first contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
-require('@nomiclabs/hardhat-waffle');
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.0",
-  networks: {
-    rinkeby: {
-      url: `${process.env.POKT_URL}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-    } 
-  }
+	solidity: "0.8.0",
+	networks: {
+		goerli: {
+			url: `${process.env.POKT_URL}`,
+			accounts: [`${process.env.PRIVATE_KEY}`],
+		},
+	},
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,18 @@
-import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const [owner] = await hre.ethers.getSigners();
+	const BankContractFactory = await hre.ethers.getContractFactory("Bank");
+	const BankContract = await BankContractFactory.deploy();
+	await BankContract.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+	console.log("BankContract deployed to: ", BankContract.address);
+	console.log("BankContract owner address: ", owner.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
